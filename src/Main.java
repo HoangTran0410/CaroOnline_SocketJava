7696c5d8c4f874b97adb5b97186239df095490a8
Hoang Tran <hoang.tran@youthdev.net>
2020-10-06 14:18:35 +0700

    basic offline gameplay

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Client.Game;
+
+
+/**
+ *
+ * @author th016
+ */
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game("CODELEARN - GAME CARO");
+    }
+}

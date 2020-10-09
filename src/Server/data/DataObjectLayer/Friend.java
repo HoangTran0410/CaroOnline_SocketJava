f6fa0482b9ecdba8ae0958087fbac5651a4510ac
Nguyễn Thiên Hữu <nguyenthienhuu18041999@gmail.com>
2020-10-09 16:27:31 +0700

    add Friend DTO file

--- src/Server/data/DataObjectLayer/Friend.java
+++ src/Server/data/DataObjectLayer/Friend.java
@@ -13,4 +13,49 @@ public class Friend {
     int ID;
     String userID;
     String friendID;
+    int totalMatchTogether;
+
+    public Friend() {
+    }
+
+    public Friend(int ID, String userID, String friendID, int totalMatchTogether) {
+        this.ID = ID;
+        this.userID = userID;
+        this.friendID = friendID;
+        this.totalMatchTogether = totalMatchTogether;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFriendID() {
+        return friendID;
+    }
+
+    public void setFriendID(String friendID) {
+        this.friendID = friendID;
+    }
+
+    public int getTotalMatchTogether() {
+        return totalMatchTogether;
+    }
+
+    public void setTotalMatchTogether(int totalMatchTogether) {
+        this.totalMatchTogether = totalMatchTogether;
+    }
+    
+    
 }

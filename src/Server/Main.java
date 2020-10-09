3e75a10929bbdfb20a4f5cdae9fc35d1d661d550
Nguyễn Thiên Hữu <nguyenthienhuu18041999@gmail.com>
2020-10-09 14:49:24 +0700

    Connect to database session

--- src/Server/Main.java
+++ src/Server/Main.java
@@ -12,22 +12,14 @@ import java.util.ArrayList;
  *
  * @author nguye
  */
-public class Caro {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String file = "src/Server/data/Player.txt";
-        ArrayList testdt = new ArrayList();
-        testdt.add("id1\tname1\tname2");
-        testdt.add("id2\thahahuhu\tname2");
-        testdt.add("id3\tname1\thehehe");
-        testdt.add("id4\tname1\tname2");
-        dataFilesConnect conn = new dataFilesConnect(file);
-        conn.reWrite(testdt,true);
-        System.out.println(conn.read());
+        MysqlConnector conn = new MysqlConnector();
         
     }
     

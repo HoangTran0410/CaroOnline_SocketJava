1647133178a1fdb982ddbad4d3b877624c0ca664
Hoang Tran <hoang.tran@youthdev.net>
2020-10-10 07:45:24 +0700

    rename folder 'data' to 'Layers'

--- src/Server/Layers/DataAccessLayer/FriendDAL.java
+++ src/Server/Layers/DataAccessLayer/FriendDAL.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Server.data.DataAccessLayer;
+package Server.Layers.DataAccessLayer;
 
 /**
  *
  * @author nguye
  */
-public class HistoryDAL {
+public class FriendDAL {
     
 }

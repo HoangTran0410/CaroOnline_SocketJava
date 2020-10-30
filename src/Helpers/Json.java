48457ae20d2ca5e8a48be3ef99faa9280572da30
Hoang Tran <hoang.tran@youthdev.net>
2020-10-30 23:06:07 +0700

    add helper folder

---
+++ src/Helpers/Json.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helpers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author Hoang Tran < hoang at 99.hoangtran@gmail.com >
+ */
+public class Json {
+
+    static JSONParser parser = new JSONParser();
+
+    public static JSONObject parse(String str) {
+        return (JSONObject) Json.parseObj(str);
+    }
+
+    public static JSONArray parseArray(String str) {
+        return (JSONArray) Json.parseObj(str);
+    }
+
+    public static Object parseObj(String str) {
+        try {
+            Object obj = parser.parse(str);
+            return obj;
+        } catch (ParseException ex) {
+            System.err.println("Error while parse json. " + ex.getMessage());
+        }
+
+        return null;
+    }
+}
---
+++ src/Helpers/Line.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helpers;
+
+/**
+ *
+ * @author Hoang Tran < hoang at 99.hoangtran@gmail.com >
+ */
+public class Line {
+
+    public int x1, x2, y1, y2;
+
+    public Line(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+}

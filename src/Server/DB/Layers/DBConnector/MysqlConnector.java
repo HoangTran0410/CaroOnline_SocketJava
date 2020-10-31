73d79aa783b2103d4518e1037458c1d494e1a18e
Nguyễn Thiên Hữu <nguyenthienhuu18041999@gmail.com>
2020-10-31 14:55:42 +0700

    change statement -> preparedStatement for dbconnection

--- src/Server/DB/Layers/DBConnector/MysqlConnector.java
+++ src/Server/DB/Layers/DBConnector/MysqlConnector.java
@@ -5,11 +5,11 @@
  */
 package Server.DB.Layers.DBConnector;
 
+import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  *
@@ -17,7 +17,7 @@ import java.sql.Statement;
  */
 public class MysqlConnector {
 
-    Statement stm = null;
+    PreparedStatement stm = null;
     Connection conn = null;
     ResultSet rs = null;
 
@@ -64,7 +64,6 @@ public class MysqlConnector {
         try {
             String url = "jdbc:mysql://" + server + "/" + db;
             conn = DriverManager.getConnection(url, user, pass);
-            stm = conn.createStatement();
             return true;
         } catch (SQLException e) {
             System.err.println("Loi ket noi DB: " + e.getMessage());
@@ -72,10 +71,10 @@ public class MysqlConnector {
         }
     }
 
-    public ResultSet sqlQry(String qry) {
+    public ResultSet sqlQry(PreparedStatement stm) {
         if (checkConnection()) {
             try {
-                rs = stm.executeQuery(qry);
+                rs = stm.executeQuery();
                 return rs;
             } catch (SQLException e) {
                 System.err.println("Loi thuc thi query !!");
@@ -84,10 +83,10 @@ public class MysqlConnector {
         return null;
     }
 
-    public boolean sqlUpdate(String qry) {
+    public boolean sqlUpdate(PreparedStatement stm) {
         if (checkConnection()) {
             try {
-                stm.executeUpdate(qry);
+                stm.executeUpdate();
                 return true;
             } catch (SQLException e) {
                 System.err.println("Gia tri cua khoa vua nhap khong ton tai !!");
@@ -96,9 +95,9 @@ public class MysqlConnector {
         }
         return false;
     }
-
+    
     public boolean checkConnection() {
-        if (conn == null || stm == null) {
+        if (conn == null ) {
             return false;
         }
         return true;
@@ -119,5 +118,10 @@ public class MysqlConnector {
             return false;
         }
     }
+
+    public Connection getConnection() {
+        return conn;
+    }
+    
 
 }

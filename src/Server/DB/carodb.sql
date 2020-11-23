88bb87413ab8721f5909e8abfe2e1696dc79d05f
HoangTran <99.hoangtran@gmail.com>
2020-11-23 11:42:26 +0700

    [Database] update database

---
+++ src/Server/DB/carodb.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 23, 2020 lúc 05:26 AM
+-- Phiên bản máy phục vụ: 10.4.16-MariaDB
+-- Phiên bản PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `carodb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `gamematch`
+--
+
+CREATE TABLE `gamematch` (
+  `ID` int(11) NOT NULL,
+  `PlayerID1` int(11) NOT NULL,
+  `PlayerID2` int(11) NOT NULL,
+  `WinnerID` int(11) DEFAULT NULL,
+  `PlayTime` int(11) NOT NULL,
+  `TotalMove` int(11) NOT NULL,
+  `StartedTime` datetime NOT NULL,
+  `Chat` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `player`
+--
+
+CREATE TABLE `player` (
+  `ID` int(11) NOT NULL,
+  `Email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Avatar` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Gender` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `YearOfBirth` int(4) DEFAULT NULL,
+  `Score` int(11) DEFAULT 0,
+  `MatchCount` int(11) DEFAULT 0,
+  `WinRate` float DEFAULT NULL,
+  `WinStreak` int(11) DEFAULT NULL,
+  `Rank` int(11) DEFAULT NULL,
+  `Blocked` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `player`
+--
+
+INSERT INTO `player` (`ID`, `Email`, `Password`, `Avatar`, `Name`, `Gender`, `YearOfBirth`, `Score`, `MatchCount`, `WinRate`, `WinStreak`, `Rank`, `Blocked`) VALUES
+(1, '99.hoangtran@gmail.com', '202CB962AC59075B964B07152D234B70', 'icons8_alien_96px.png', 'hoang tran', 'nam', 1999, 0, 0, NULL, NULL, NULL, 0),
+(2, 'hientran@gmail.com', '202CB962AC59075B964B07152D234B70', 'icons8_angry_face_meme_96px.png', 'thu hien', 'nữ', 1999, 0, 0, NULL, NULL, NULL, 0);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `gamematch`
+--
+ALTER TABLE `gamematch`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Chỉ mục cho bảng `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `UNIQUE` (`Email`) USING BTREE;
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `gamematch`
+--
+ALTER TABLE `gamematch`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `player`
+--
+ALTER TABLE `player`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
